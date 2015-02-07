e1d1e30efa802b198e532438c0621fb0a9ffca57
CF Grugan <charles_grugan@cable.comcast.com>
2015-02-07 13:53:21 -0500

    adding mouse move polling to remove and show thumbnails

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,27 @@
   var mainWindow = document.getElementById('mainWindow');
+  var thumbContainer = document.getElementById('thumbContainer');
+  var idletime = 0;
 
+  window.addEventListener('mousemove', function() {
+    idleTime = 0;
+  });
+
+  function pollMouseMove() {
+    var delay = 10;
+    if (this.idleTime >= 2) {
+      this.thumbContainer.classList.remove('shown');
+    } else {
+      this.thumbContainer.classList.add('shown');
+    }
+    if (this.idleTime < 2) {
+      delay = 1000;
+      this.idleTime = this.idleTime + 1;
+    }
+
+    setTimeout(function() {
+       pollMouseMove();
+     }, delay);
+  }
   function updateSelectedMedia(sender){
     var str = sender.src.replace('thumbnails', 'fullsize');
     str = str.replace('png', 'jpg');
@@ -10,3 +32,5 @@
       mainWindow.classList.remove('transitioning');
     }, 800);
   }
+
+  pollMouseMove();
