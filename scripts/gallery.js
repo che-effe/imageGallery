60fc99cfb53850234af5473b5b4c96dec08ddcdc
CF Grugan <charles_grugan@cable.comcast.com>
2015-04-29 19:39:48 -0400

    making some style changes

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,37 +1,38 @@
-  var mainWindow = document.getElementById('mainWindow');
-  var thumbContainer = document.getElementById('thumbContainer');
-  var idletime = 0;
+var mainWindow = document.getElementById('mainWindow');
+var thumbContainer = document.getElementById('thumbContainer');
+var idletime = 0;
 
-  window.addEventListener('mousemove', function() {
-    idleTime = 0;
-  });
+window.addEventListener('mousemove', function() {
+  idleTime = 0;
+});
 
-  function pollMouseMove() {
-    var delay = 10;
-    if (this.idleTime >= 2) {
-      this.thumbContainer.classList.remove('shown');
-    } else {
-      this.thumbContainer.classList.add('shown');
-    }
-    if (this.idleTime < 2) {
-      delay = 1000;
-      this.idleTime = this.idleTime + 1;
-    }
-
-    setTimeout(function() {
-       pollMouseMove();
-     }, delay);
+function pollMouseMove() {
+  var delay = 10;
+  if (this.idleTime >= 2) {
+    this.thumbContainer.classList.remove('shown');
+  } else {
+    this.thumbContainer.classList.add('shown');
   }
 
-  function updateSelectedMedia(sender){
-    var str = sender.src.replace('thumbnails', 'fullsize');
-    str = str.replace('png', 'jpg');
-    mainWindow.classList.add('transitioning');
-
-    setTimeout( function() {
-      mainWindow.src = str;
-      mainWindow.classList.remove('transitioning');
-    }, 5s00);
+  if (this.idleTime < 2) {
+    delay = 1000;
+    this.idleTime = this.idleTime + 1;
   }
 
-  pollMouseMove();
+  setTimeout(function() {
+     pollMouseMove();
+   }, delay);
+}
+
+function updateSelectedMedia(sender) {
+  var str = sender.src.replace('thumbnails', 'fullsize');
+  str = str.replace('png', 'jpg');
+  mainWindow.classList.add('transitioning');
+
+  setTimeout(function() {
+    mainWindow.src = str;
+    mainWindow.classList.remove('transitioning');
+  }, 500);
+}
+
+pollMouseMove();
